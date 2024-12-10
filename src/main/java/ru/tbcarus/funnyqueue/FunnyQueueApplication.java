fd269928bf365d114c9acaeab0a07ecde61d6607
tbcarus <70680359+tbcarus@users.noreply.github.com>
2024-12-10 06:30:45 +0300

    Initial commit

---
+++ src/main/java/ru/tbcarus/funnyqueue/FunnyQueueApplication.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.funnyqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunnyQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tbcarus/funnyqueue/FunnyQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.funnyqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
