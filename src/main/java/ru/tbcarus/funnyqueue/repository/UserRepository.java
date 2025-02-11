024bf7e4c81259c427933edda0ed3cb1c4c184a2
tbcarus <70680359+tbcarus@users.noreply.github.com>
2025-02-11 22:15:46 +0300

    Commit to transfer

---
+++ src/main/java/ru/tbcarus/funnyqueue/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package ru.tbcarus.funnyqueue.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import ru.tbcarus.funnyqueue.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findUserByEmail(String Email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.enabled = NOT u.enabled WHERE u.id = :id")
+    void reverseEnableUser(int id);
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r = 'SUPERUSER'")
+    List<User> findAllQueueOwners();
+}
